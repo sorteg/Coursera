27d06ac191012fc711c460717ed601186cda4218
Susana Ortega <sorteg@umich.edu>
2019-06-29 18:30:50 -0500

    week5 completed

---
+++ Web_Applications_for_Everybody/Building_Web_Applications_in_PHP/week5/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Susana Ortega PHP </title>
+</head>
+
+<body>
+<h1>Susana Ortega PHP</h1>
+<pre>ASCII ART:
+    *****
+   *     *
+   *
+    ****
+        *
+  *     *
+   *****
+</pre>
+
+<p>The SHA256 hash of "Susana Ortega" is
+<?PHP
+  print hash('sha256', 'Susana Ortega');
+?>
+</p>
+
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
